05fb9008a1b1af0660be746c45b7b35c06ae94bb
Nick Neuman <nick.neuman@yale.edu>
2016-10-06 18:34:32 +0000

    add nickname

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
+app.get('/gifted-bat', function(request, response){
+    response.send('you are a gifted bat!');
+});
+
 app.listen(process.env.PORT || 4000);
